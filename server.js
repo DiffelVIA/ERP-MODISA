5a71f3a619c0798b36049aa57be48d2cdbc19691
Diffel Villalva <diffelv@gmail.com>
2026-08-13 11:33:33 -0600

    refactor(core): extraer endpoints monoliticos de server.js hacia arquitectura modular de routers

--- server.js
+++ server.js
@@ -1,74 +1,42 @@
-require('dotenv').config(); // db
+require('dotenv').config();
 const express = require('express');
-const mysql = require('mysql2/promise'); // db
 const cors = require('cors');
-const fs = require('fs'); // db
-const path = require('path'); // db
-const bcrypt = require('bcrypt'); 
+const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// CONEXIÓN A MYSQL//
-const pool = require('./src/config/db');
-
-// DRIVE //
+// Importación de Routers
 const authRouter = require('./src/routes/auth');
-app.use('/api/auth/google', authRouter);
-
-// MULTER //
-const multer = require('multer');
-
-// UPLOAD //
-const upload = require('./src/middlewares/uploads');
-
-// INICIO DE SESIÓN Y GESTIÓN DE CONTRASEÑA //
-app.use('/api/auth', authRouter);
-
-// PROYECTOS //
 const projectsRouter = require('./src/routes/projects');
+const categoriesRouter = require('./src/routes/categories');
+const employeesRouter = require('./src/routes/employees');
+const minutesRouter = require('./src/routes/minutes');
+const materialesRouter = require('./src/routes/materials');
+const creditosRouter = require('./src/routes/credits');
+const contratosRouter = require('./src/routes/contracts');
+const pagosRouter = require('./src/routes/payments');
+const dashboardRouter = require('./src/routes/dashboard');
 
-app.use('/api/projects', projectsRouter);
+// Vinculación de Rutas
+app.use('/api/auth/google', authRouter);
+app.use('/api/auth', authRouter);
 app.use('/api/proyectos', projectsRouter);
 app.use('/api/projects-report', (req, res, next) => {
   req.url = '/report' + req.url;
   projectsRouter(req, res, next);
 });
-
-// CATEGORÍZACIÓN //
-const categoriesRouter = require('./src/routes/categories');
 app.use('/api', categoriesRouter);
-
-// EMPLEADOS //
-const employeesRouter = require('./src/routes/employees');
 app.use('/api/empleados', employeesRouter);
-
-// MINUTAS //
-const minutesRouter = require('./src/routes/minutes');
 app.use('/api', minutesRouter);
-
-// MATERIALES //
-const materialesRouter = require('./src/routes/materials');
 app.use('/api/materiales', materialesRouter);
-
-//CREDITOS //
-const creditosRouter = require('./src/routes/credits');
 app.use('/api/creditos', creditosRouter);
-
-// CONTRATOS
-const contratosRouter = require('./src/routes/contracts');
 app.use('/api/contratos', contratosRouter);
-
-// PAGOS //
-const pagosRouter = require('./src/routes/payments');
 app.use('/api/pagos', pagosRouter);
-
-// DASHBOARD //
-const dashboardRouter = require('./src/routes/dashboard');
 app.use('/api/dashboard', dashboardRouter);
 
-// ARRANQUE DE SERVIDOR //
+// Arranque de Servidor
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
