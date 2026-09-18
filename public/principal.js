c42df9a5474087543e022014794d82c9f4ccf9cb
Diffel Villalva <diffelv@gmail.com>
2026-09-18 11:47:21 -0600

    l

--- public/principal.js
+++ public/principal.js
@@ -55,7 +55,7 @@
             
             const rolesPermitidosEmpleados = [
                 'director operativo',
-                'gerente de administración',
+                'gerente administración',
                 'compras'
             ];
 
